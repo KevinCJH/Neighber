66f6ae01c1e147fc413972592acd17d352d47efb
Kevin Chin <KevinCJH@users.noreply.github.com>
2017-06-23 17:24:32 +0800

    Kevinbranch (#4)
    * testbranch
    
    * testbranch
    
    * updated registration and log in

--- app/src/main/java/orbital/raspberry/neighber/User.java
+++ app/src/main/java/orbital/raspberry/neighber/User.java
@@ -1,14 +1,39 @@
 package orbital.raspberry.neighber;
 
-/**
- * Created by Kevin Chin on 19/6/2017.
- */
-
 public class User {
 
-    private String username;
+    private String userid;
+    private String displayname;
     private String email;
     private double ratings;
-    
+
+    public User(String userid, String email, String displayname){
+
+        this.userid = userid;
+        this.email = email;
+        this.displayname = displayname;
+        ratings = 0;
+
+    }
+
+    public String getUserid(){
+        return userid;
+    }
+
+    public String getDisplayname(){
+        return displayname;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public double getRatings(){
+        return ratings;
+    }
+
+    public void setRatings(double ratings){
+        this.ratings = ratings;
+    }
 
 }
