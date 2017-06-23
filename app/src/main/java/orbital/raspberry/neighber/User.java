855e953ef4cfaad41bd64028ed69a727d769f5e6
KEVIN CHIN <kevinchinjh@gmail.com>
2017-06-23 12:10:14 +0800

    testbranch

---
+++ app/src/main/java/orbital/raspberry/neighber/User.java
@@ -0,0 +1,14 @@
+package orbital.raspberry.neighber;
+
+/**
+ * Created by Kevin Chin on 19/6/2017.
+ */
+
+public class User {
+
+    private String username;
+    private String email;
+    private double ratings;
+    
+
+}
