7499c673d137f4901a114fe8ec80ce20e04176d9
Kevin Chin <KevinCJH@users.noreply.github.com>
2017-08-20 22:56:21 +0800

    Kevinbranch (#27)
    * testbranch
    
    * testbranch
    
    * updated registration and log in
    
    * done profile CRUD
    
    * done add request
    
    * done add request
    
    * done browse request
    
    * done browse offer
    
    * make offer almost completed
    
    * records almost completed
    
    * history almost completed
    
    * setting almost completed
    
    * chat in progress
    
    * help in progress
    
    * done search
    
    * improvements made to UI
    
    * add confirmation for del
    
    * add confirmation for del completed
    
    * improvements to search function
    
    * ui improvement
    
    * ui improvement
    
    * ui improvement
    
    * backend system overhaul
    
    * backend system overhaul completed
    
    * UI overhaul complete
    
    * action bar updated
    
    * added camera feature
    
    * chat update
    
    * chat encryption implemented
    
    * alot of features added
    
    * floating action button implemented. improved search. complete cycle system to introduce favourite function
    
    * favourites and feedback function added
    
    * favourites and feedback function added, added price
    
    * minor update to make fee truly optional
    
    * minor update to make fee truly optional
    
    * bug fixes
    
    * minor ui fix
    
    * Added App Introduction

--- app/src/main/java/orbital/raspberry/neighber/LoginpageActivity.java
+++ app/src/main/java/orbital/raspberry/neighber/LoginpageActivity.java
@@ -2,7 +2,9 @@ package orbital.raspberry.neighber;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -31,6 +33,46 @@ public class LoginpageActivity extends AppCompatActivity {
 
         //Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
+
+        /////////////////////////  Declare a new thread to do a preference check/////////////////////////////
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //  Initialize SharedPreferences
+                SharedPreferences getPrefs = PreferenceManager
+                        .getDefaultSharedPreferences(getBaseContext());
+
+                //  Create a new boolean and preference and set it to true
+                boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
+
+                //  If the activity has never started before...
+                if (isFirstStart) {
+
+                    //  Launch app intro
+                    final Intent i = new Intent(LoginpageActivity.this, IntroActivity.class);
+
+                    runOnUiThread(new Runnable() {
+                        @Override public void run() {
+                            startActivity(i);
+                        }
+                    });
+
+                    //  Make a new preferences editor
+                    SharedPreferences.Editor e = getPrefs.edit();
+
+                    //  Edit preference to make it false because we don't want this to run again
+                    e.putBoolean("firstStart", false);
+
+                    //  Apply changes
+                    e.apply();
+                }
+            }
+        });
+
+        // Start the thread
+        t.start();
+
+        ///////////////////////////////////////////////////////////////
 
         if (auth.getCurrentUser() != null) {
             startActivity(new Intent(LoginpageActivity.this, MainActivity.class));
